1494df9f42d8c15ef35173992d846bbdf2d9723c
Michał Poręba <michalporeba@gmail.com>
2023-05-15 12:58:06 -0400

    exploring the stOTTR visitor

--- pyottr/ottr.py
+++ pyottr/ottr.py
@@ -1,41 +1,7 @@
 from antlr4 import InputStream, CommonTokenStream
 from .grammar.stOTTRLexer import stOTTRLexer
 from .grammar.stOTTRParser import stOTTRParser
-from .grammar.stOTTRVisitor import stOTTRVisitor
-from .grammar.TurtleLexer import TurtleLexer
-from .grammar.TurtleParser import TurtleParser
-from .grammar.TurtleVisitor import TurtleVisitor
-
-
-class MyStottrVisitor(stOTTRVisitor):
-    def visitStatement(self, ctx):
-        print(f"Visited statement: {ctx.getText()}")
-        return super().visitStatement(ctx)
-
-    # Visit a parse tree produced by stOTTRParser#signature.
-    def visitSignature(self, ctx:stOTTRParser.SignatureContext):
-        print(f"Visited signature: {ctx.getText()}")
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by stOTTRParser#templateName.
-    def visitTemplateName(self, ctx:stOTTRParser.TemplateNameContext):
-        print(f"Visited template name: {ctx.getText()}")
-        return self.visitChildren(ctx)
-
-class MyTurtleVisitor(TurtleVisitor):
-
-    # Visit a parse tree produced by TurtleParser#directive.
-    def visitDirective(self, ctx:TurtleParser.DirectiveContext):
-        print(f"Visited directive: {ctx.getText()}")
-        return self.visitChildren(ctx)
-
-    # Visit a parse tree produced by TurtleParser#prefixID.
-    def visitPrefixID(self, ctx:TurtleParser.PrefixIDContext):
-        print(f"Visited prefix ID: {ctx.getText()}")
-        return self.visitChildren(ctx)
-
-
+from .stOTTRVisitor import stOTTRVisitor
 
 def tryit():
     stottr_input = """
@@ -55,16 +21,7 @@ def tryit():
     parser = stOTTRParser(token_stream)
     parse_tree = parser.stOTTRDoc()
 
-    ttl_stream = InputStream(stottr_input)
-    ttl_lexer = TurtleLexer(ttl_stream)
-    ttl_token_stream = CommonTokenStream(ttl_lexer)
-    ttl_parser = TurtleParser(ttl_token_stream)
-    ttl_parse_tree = ttl_parser.turtleDoc()
-
-    ttl_visitor = MyTurtleVisitor()
-    ttl_visitor.visit(ttl_parse_tree)
-
-    visitor = MyStottrVisitor()
+    visitor = stOTTRVisitor()
     result = visitor.visit(parse_tree)
 
     print(result)
--- pyottr/stOTTRVisitor.py
+++ pyottr/stOTTRVisitor.py
@@ -22,3 +22,208 @@ class stOTTRVisitor(BaseVisitor):
         print(f"Visited template name: {ctx.getText()}")
         self.templates[ctx.getText()] = Template(ctx.getText())
         return self.visitChildren(ctx)
+    
+
+    # Visit a parse tree produced by stOTTRParser#parameterList.
+    def visitParameterList(self, ctx:stOTTRParser.ParameterListContext):
+        print(f"Visited parameter list: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#parameter.
+    def visitParameter(self, ctx:stOTTRParser.ParameterContext):
+        print(f"Visited parameter: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#defaultValue.
+    def visitDefaultValue(self, ctx:stOTTRParser.DefaultValueContext):
+        print(f"Visited default value: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#annotationList.
+    def visitAnnotationList(self, ctx:stOTTRParser.AnnotationListContext):
+        print(f"Visited annotation list: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#annotation.
+    def visitAnnotation(self, ctx:stOTTRParser.AnnotationContext):
+        print(f"Visited annotation: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#baseTemplate.
+    def visitBaseTemplate(self, ctx:stOTTRParser.BaseTemplateContext):
+        print(f"Visited base template : {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#template.
+    def visitTemplate(self, ctx:stOTTRParser.TemplateContext):
+        print(f"Visited template: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#patternList.
+    def visitPatternList(self, ctx:stOTTRParser.PatternListContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#instance.
+    def visitInstance(self, ctx:stOTTRParser.InstanceContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#argumentList.
+    def visitArgumentList(self, ctx:stOTTRParser.ArgumentListContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#argument.
+    def visitArgument(self, ctx:stOTTRParser.ArgumentContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#type.
+    def visitType(self, ctx:stOTTRParser.TypeContext):
+        print(f"Visited type: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#listType.
+    def visitListType(self, ctx:stOTTRParser.ListTypeContext):
+        print(f"Visited list type: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#neListType.
+    def visitNeListType(self, ctx:stOTTRParser.NeListTypeContext):
+        print(f"Visited NeListType: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#lubType.
+    def visitLubType(self, ctx:stOTTRParser.LubTypeContext):
+        print(f"Visited LubType: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#basicType.
+    def visitBasicType(self, ctx:stOTTRParser.BasicTypeContext):
+        print(f"Visited basic type: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#term.
+    def visitTerm(self, ctx:stOTTRParser.TermContext):
+        print(f"Visited term {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#constantTerm.
+    def visitConstantTerm(self, ctx:stOTTRParser.ConstantTermContext):
+        print(f"Visited constant term: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#constant.
+    def visitConstant(self, ctx:stOTTRParser.ConstantContext):
+        print(f"Visited constant: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#none.
+    def visitNone(self, ctx:stOTTRParser.NoneContext):
+        print(f"Visited none: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#termList.
+    def visitTermList(self, ctx:stOTTRParser.TermListContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#constantList.
+    def visitConstantList(self, ctx:stOTTRParser.ConstantListContext):
+        print(f"Visited constant list: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#turtleDoc.
+    def visitTurtleDoc(self, ctx:stOTTRParser.TurtleDocContext):
+        print(f"Visited turtle doc: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#directive.
+    def visitDirective(self, ctx:stOTTRParser.DirectiveContext):
+        print(f"Visited directive: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#prefixID.
+    def visitPrefixID(self, ctx:stOTTRParser.PrefixIDContext):
+        print(f"Visited prefix ID: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#base.
+    def visitBase(self, ctx:stOTTRParser.BaseContext):
+        print(f"Visited base: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#sparqlBase.
+    def visitSparqlBase(self, ctx:stOTTRParser.SparqlBaseContext):
+        print(f"Visited SPARQL base: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#sparqlPrefix.
+    def visitSparqlPrefix(self, ctx:stOTTRParser.SparqlPrefixContext):
+        print(f"Visited SPARQL prefix: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#literal.
+    def visitLiteral(self, ctx:stOTTRParser.LiteralContext):
+        print(f"Visited literal: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#numericLiteral.
+    def visitNumericLiteral(self, ctx:stOTTRParser.NumericLiteralContext):
+        print(f"Visited numeric literal: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#rdfLiteral.
+    def visitRdfLiteral(self, ctx:stOTTRParser.RdfLiteralContext):
+        print(f"Visited rdf literal: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#iri.
+    def visitIri(self, ctx:stOTTRParser.IriContext):
+        print(f"Visited iri: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#prefixedName.
+    def visitPrefixedName(self, ctx:stOTTRParser.PrefixedNameContext):
+        print(f"Visited prefixed name: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#blankNode.
+    def visitBlankNode(self, ctx:stOTTRParser.BlankNodeContext):
+        print(f"Visited blank node: {ctx.getText()}")
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by stOTTRParser#anon.
+    def visitAnon(self, ctx:stOTTRParser.AnonContext):
+        print(f"Visited anon: {ctx.getText()}")
+        return self.visitChildren(ctx)
