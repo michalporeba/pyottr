d42bf405a76c1bcf52a134cf865246fb5c5ca31e
Michał Poręba <michalporeba@gmail.com>
2023-05-15 07:55:43 -0400

    parsing names from multiple templates

--- tests/test_stottr.py
+++ tests/test_stottr.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 from pyottr.stOTTR import stOTTR
 
+
+TEMPLATE_SIGNATURES = """
+    ex:Template1 [ ?a , ?b ] .
+    ex:Template2 [ ! owl:Class ?a, ? xsd:int ?b = 5 ] .
+    ex:Template3 [ !??a ] .
+    """
+
+
 class StottrShould(TestCase):
 
     def test_single_minimal_template(self):
@@ -8,3 +16,11 @@ class StottrShould(TestCase):
         stottr.process("ex:EmptyTemplate [ ] .")
         assert 1 == len(stottr.templates)
         assert None != stottr.templates['ex:EmptyTemplate']
+
+    def test_names_from_templates(self):
+        stottr = stOTTR()
+        stottr.process(TEMPLATE_SIGNATURES)
+        assert 3 == len(stottr.templates)
+        assert None != stottr.templates['ex:Template1']
+        assert None != stottr.templates['ex:Template2']
+        assert None != stottr.templates['ex:Template3']
