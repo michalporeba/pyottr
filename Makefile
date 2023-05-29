f18e1f88281a2f4f780847cf1bd9e03017d4ccbc
Michał Poręba <michalporeba@gmail.com>
2023-05-29 11:53:00 +0100

    upgrading antlr to 4.13

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 grammar:
 	rm -rf ./pyottr/grammar/*
 	cd antlr && \
-		java -jar antlr-4.12.0-complete.jar \
+		java -jar antlr-4.13.0-complete.jar \
 		-Dlanguage=Python3 -visitor -no-listener -o ../pyottr/grammar/ \
 		Turtle.g4 stOTTR.g4 
 
--- pyottr/grammar/TurtleVisitor.py
+++ pyottr/grammar/TurtleVisitor.py
@@ -1,6 +1,6 @@
-# Generated from Turtle.g4 by ANTLR 4.12.0
+# Generated from Turtle.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .TurtleParser import TurtleParser
 else:
     from TurtleParser import TurtleParser
