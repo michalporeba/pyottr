c1e5a84f8dd529ea7f1df761453b0d488807e8d8
Michał Poręba <michalporeba@gmail.com>
2023-05-30 22:37:15 +0100

    testing deeper nesting

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -104,3 +104,26 @@ def test_processing_pizzas_from_data_generator():
         'p:Hawaii rdfs:label "Hawaii"',
     ]
     assert triples == expected
+
+
+def test_deeper_nesting():
+    sut = PyOTTR()
+    rdf = list(sut.process("""
+    ex:A[?x] :: {
+        ottr:Triple(p:Test, rdfs:label, ?x), 
+        ottr:Triple(p:Test, rdfs:label, "A") 
+    } .
+    ex:B[?x] :: { ex:A(?x) } .
+    ex:C[?x] :: { ex:B(?x), ottr:Triple(p:Test, rdfs:label, "C") } .
+    ex:D[?x] :: { ex:C(?x) } .
+    
+    ex:D("Test Argument") .
+    """))
+
+    expected = [
+        'p:Test rdfs:label "Test Argument"',
+        'p:Test rdfs:label "A"',
+        'p:Test rdfs:label "C"'
+    ]
+
+    assert rdf == expected
